14374b1ad3c8fac1723620abc8a777c203b110b5
James <49901378+Mifuu@users.noreply.github.com>
2022-11-25 06:50:52 +0700

    update

--- Assets/Scripts/PlayMode/InputProcess.cs
+++ Assets/Scripts/PlayMode/InputProcess.cs
@@ -41,7 +41,7 @@ public class InputProcess : MonoBehaviour
 
         if (GameManager.nextSceneMode == GameManager.NextSceneMode.Multi) {
             // calling deliverMsg to send message in JSON format when the typed word matches
-            if (WordManager.CheckWord(word, false)) {
+            if (WordManager.CheckWord(word, true)) {
                 ConnectionManager.DeliverMsg("playerTyped", word);
                 SFXManager._PlaySFX("Right1", gameObject);
             } else {
